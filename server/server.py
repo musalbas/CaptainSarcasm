c2dd74d07ed2919398d2355d4256a571199ba2ad
Mustafa <mus@musalbas.com>
2015-04-11 17:34:16 +0100

    api werks

--- server/server.py
+++ server/server.py
@@ -1,12 +1,13 @@
+from flask import request
 from flask import Flask
 
 from detector import evaluate
 
 app = Flask(__name__)
 
-@app.route('/api')
+@app.route('/api', methods=['POST'])
 def api():
-    return "hi"
+    return str(evaluate.tweetscore(request.form['message']))
 
 if __name__ == "__main__":
     app.run()
